1e16e129fea07f0f55c88cdfb703bf5f44eeece1
Benjamin23 <benjamin_suk@yahoo.com>
2015-08-24 15:24:49 +0200

    ..

--- public/scripts/uploadController.js
+++ public/scripts/uploadController.js
@@ -48,40 +48,40 @@ app.controller('UploadController', ['$scope', 'Upload', '$timeout', function ($s
                 }
             }
 
-            //Moguce je i array slati, ali zbog obrade na serveru je jednostavnije ovako
-            for (var i = 0; i < numberOfFiles; i++) {
-
-                var file = files[i];
-                //Moguce i drugacije uploadati, ovo je nacin preko kontolera, a drugi je preko servisa
-                Upload.upload({
-                    url: '/upload',
-                    method: 'POST',
-                    fields: {
-                        'username': 'BenjaminFields',
-                        'password': 'Benjamin Pass',
-                        'nekaTrecaVar':'Neka treca'
-                    },
-
-                    file: file
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    $scope.log = 'progress: ' + progressPercentage + '% ' +
-                        evt.config.file.name + '\n' + $scope.log;
-                }).success(function (data, status, headers, config) {
-                    $timeout(function () {
-                       // $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
-                        console.log(data);
-                    });
-                }).error(function (data, status, headers, config) {
-                    $scope.errors.push(data);
-                }).xhr(function (xhr) {
-
-                    xhr.addEventListener("loadend", function () {
-                      //  alert("Upload done");
-                    }, false);
-
-                });
-            }
+            ////Moguce je i array slati, ali zbog obrade na serveru je jednostavnije ovako
+            //for (var i = 0; i < numberOfFiles; i++) {
+            //
+            //    var file = files[i];
+            //    //Moguce i drugacije uploadati, ovo je nacin preko kontolera, a drugi je preko servisa
+            //    Upload.upload({
+            //        url: '/upload',
+            //        method: 'POST',
+            //        fields: {
+            //            'username': 'BenjaminFields',
+            //            'password': 'Benjamin Pass',
+            //            'nekaTrecaVar':'Neka treca'
+            //        },
+            //
+            //        file: file
+            //    }).progress(function (evt) {
+            //        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            //        $scope.log = 'progress: ' + progressPercentage + '% ' +
+            //            evt.config.file.name + '\n' + $scope.log;
+            //    }).success(function (data, status, headers, config) {
+            //        $timeout(function () {
+            //           // $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
+            //            console.log(data);
+            //        });
+            //    }).error(function (data, status, headers, config) {
+            //        $scope.errors.push(data);
+            //    }).xhr(function (xhr) {
+            //
+            //        xhr.addEventListener("loadend", function () {
+            //          //  alert("Upload done");
+            //        }, false);
+            //
+            //    });
+            //}
         }
     };
 
